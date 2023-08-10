e6c565cab515d08c5c6539c3edd8bb77e8cb5263
bginside-dev <bginside.dev@gmail.com>
2023-08-10 19:40:04 +0800

    add json limit

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,15 @@ import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+import { json, urlencoded } from 'express'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.setGlobalPrefix('api/')
   app.useGlobalPipes(new ValidationPipe())
+
+  app.use(json({ limit: '50mb' }))
+  app.use(urlencoded({ extended: true, limit: '50mb' }))
 
   const config = new DocumentBuilder().setTitle('Gateway').setDescription('Gateway Service API').setVersion('0.1').build()
 
